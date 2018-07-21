93dee86f8d77207c798273efd64eaae023a115eb
Lucas Malta <lucas50xd@hotmail.com>
2018-07-21 16:15:50 -0300

    adds sidebar

--- screens/Sidebar.js
+++ screens/Sidebar.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { StyleSheet, Dimensions } from 'react-native';
+import { StyleSheet, Dimensions, View } from 'react-native';
 import { Text } from 'react-native-elements';
 import {
     Container,
     Content,
+    Icon,
+    List,
+    ListItem,
 } from 'native-base';
 
 
@@ -12,7 +15,35 @@ export default class Sidebar extends React.Component {
         return (
             <Container style={styles.container}>
                 <Content>
-                    <Text>Hi</Text>
+                    <View style={styles.header}>
+                        <Icon style={{ color: 'white', fontSize: 50 }} name="contact" />
+                        <Text style={{
+                            color: 'white',
+                            fontSize: 30
+                        }}> Visitante</Text>
+                    </View>
+                    <List style={styles.list}>
+                        <ListItem button noIndent style={styles.listItem}>
+                            <Text style={styles.listText}>Favoritos</Text>
+                            <Icon style={{color: 'white'}} name='arrow-forward' />
+                        </ListItem>
+                        <ListItem button noIndent>
+                            <Text style={styles.listText}>Pedidos</Text>
+                            <Icon style={{color: 'white'}} name='arrow-forward' />
+                        </ListItem >
+                        <ListItem button noIndent>
+                            <Text style={styles.listText}>Cupons</Text>
+                            <Icon style={{color: 'white'}} name='arrow-forward' />
+                        </ListItem>
+                        <ListItem button noIndent>
+                            <Text style={styles.listText}>Configurações</Text>
+                            <Icon style={{color: 'white'}} name='arrow-forward' />
+                        </ListItem>
+                        <ListItem button noIndent>
+                            <Text style={styles.listText}>Ajuda</Text>
+                            <Icon style={{color: 'white'}} name='arrow-forward' />
+                        </ListItem>
+                    </List>
                 </Content>
             </Container>
         );
@@ -24,8 +55,23 @@ let windowWidth = Dimensions.get('window').width;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'red',
+        backgroundColor: '#eb2023',
         alignItems: 'center',
         justifyContent: 'center',
     },
+    header: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        marginTop: 25,
+    },
+    list: {
+        marginTop: 25
+    },
+    listText: {
+        color: 'white',
+        fontSize: 20,
+        width: '80%'
+    }
 });
